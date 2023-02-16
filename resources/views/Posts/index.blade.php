f97a2308a1545e48ed9171c7e6b55845b2d97b2b
Suman <sgurun26@myune.edu.au>
2023-02-16 14:00:00 +0100

    Update

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Blog Posts</h1>
-    <a href="{{ route('posts.create') }}" class="btn btn-primary">Create Post</a>
-    <ul>
+
+<div class="container">
+<div class="d-flex justify-content-between align-items-center my-3">
+    <h1>Blog Posts</h1>
+    <a href="{{ route('posts.create') }}" class="btn btn-secondary">Create Post</a>
+</div>
+    <ul class="list-unstyled d-flex flex-wrap">
         @foreach ($posts as $post)
-            <li>
-                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}
+            <li class="m-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <p class="card-text">{{ $post->content }}</p>
+                        <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">Read more..</a>
+                    </div>
+                </div>
             </li>
         @endforeach
     </ul>
+</div>
 @endsection
--- resources/views/Posts/show.blade.php
+++ resources/views/Posts/show.blade.php
@@ -1,66 +1,24 @@
 @extends('layouts.app')
 @section('content')
+<div class="container">
+<div class="d-flex justify-content-between align-items-center my-3">
     <h1>Blog Post Details</h1>
+    <div class="d-flex align-items-center gap-2">
     <a href="{{ url()->previous() }}" class="btn btn-secondary">Back</a>
     <form action="{{ route('posts.destroy', $post->id) }}" method="post">
                       @csrf
                       @method('DELETE')
                       <button type="submit" class="btn btn-danger btn-sm">Delete</button>
     </form>
-    <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-info">Update</a>
+    <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-info">Updat</a>
+</div>
+</div>
     
     <ul>
         <li>ID: {{ $post->id }}</li>
         <li>Title: {{ $post->title }}</li>
         <li>Content: {{ $post->content }}</li>
     </ul>
-    {{-- <div class="container">
-        <!-- Content here -->
-      
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-8 col-lg-6">
-          <div class="card shadow-0 border" style="background-color: #f0f2f5;">
-            <div class="card-body p-4">
-                <a href="#">Back</a>
-                <h4>Blog Post Details</h4>
-                <div class="card mb-4">
-                    <div class="card-body">
-                      <p>1 - Post Title</p>
-          
-                      <div class="d-flex justify-content-between">
-                        <div class="d-flex flex-row align-items-center">
-                          <p>post content</p>
-                        </div>
-                        <div class="d-flex flex-row align-items-center">
-                          <p class="small mb-0 ms-2">Author: Ibrahim</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+</div>
 
-
-              <div data-mdb-input-init class="form-outline mb-4">
-                <input type="text" id="addANote" class="form-control" placeholder="Type comment..." />
-                <label class="form-label" for="addANote"><input type="submit" name="submit" class="mt-2"/> </label>
-              </div>
-      
-              <div class="card mb-4">
-                <div class="card-body">
-                  <p>Type your note, and hit enter to add it</p>
-      
-                  <div class="d-flex justify-content-between">
-                    <div class="d-flex flex-row align-items-center">
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp" alt="avatar" width="25"
-                        height="25" />
-                      <p class="small mb-0 ms-2">Martha</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-      
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> --}}
 @endsection
